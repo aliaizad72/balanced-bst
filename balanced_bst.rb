1330c17a11c2b644096cf1ec6bde9caeea5158e6
Ali Aizad <aliaizad72@gmail.com>
2024-03-07 11:35:11 +0800

    Complete insert method

--- balanced_bst.rb
+++ balanced_bst.rb
@@ -35,13 +35,29 @@ class Tree
     last = array.length - 1
     mid = (0 + last) / 2
 
-    return nil if 0 > last
+    return nil if 0 > last # rubocop:disable Style/NumericPredicate
 
     root = Node.new(array[mid])
     root.left = breed_the_nodes(array[0, mid])
     root.right = breed_the_nodes(array[mid + 1..last])
 
     root
+  end
+
+  def insert(node = @root, new_node)
+    if new_node < node.value
+      if node.left.nil?
+        node.left = Node.new(new_node)
+        return
+      end
+      insert(node.left, new_node)
+    elsif new_node > node.value
+      if node.right.nil?
+        node.right = Node.new(new_node)
+        return
+      end
+      insert(node.right, new_node)
+    end
   end
 
   def pretty_print(node = @root, prefix = '', is_left: true)
@@ -51,4 +67,7 @@ class Tree
   end
 end
 
-Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]).pretty_print
+tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+tree.insert(20)
+tree.insert(24)
+tree.pretty_print
